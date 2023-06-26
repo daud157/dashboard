b1b5d7175e63102395c2dcd518f0120f32172acb
daud157 <daudahmed157@gmail.com>
2023-06-26 09:46:15 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+    
+    }
+  },
+  plugins: [],
+}
+module.exports = {
+  theme: {
+    extend: {
+      gridRow: {
+        'span-16': 'span 16 / span 16',
+      }
+    }
+ }
+}
